8323f1270cf60b82189e3eb09a12f53648965712
Nikhil Kumar Singh <nikhilksingh97@gmail.com>
2016-08-17 11:17:10 -0700

    Update hackerearthapi.py

--- hackerearthapi.py
+++ hackerearthapi.py
@@ -1,7 +1,7 @@
 import requests
 
 RUN_URL = u'https://api.hackerearth.com/v3/code/run/'
-CLIENT_SECRET = '0a7f0101e5cc06e4417a3addeb76164680ac83a4'
+CLIENT_SECRET = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 
 def run_code(lang,code,inputs):
@@ -22,9 +22,4 @@ def run_code(lang,code,inputs):
 
 
 
-'''
-Name: nikhil-123
-Hostname: http://whatsup.pythonanywhere.com/
-Client Id: bcad7a43c0d562aeef8f85d688fec2718edf0622b90a.api.hackerearth.com
-Client Secret Key: 0fe5b082e17d60fca14d55dc1f4a1922778c3d3c
-'''
+
